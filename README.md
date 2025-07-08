15e495ca48f790243a94b9630c8b265af662288b
Morgan Ney <morganney@gmail.com>
2025-07-07 20:16:09 -0500

    chore: repo setup.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Light Switch
+
+Turn your web app's lights off.
+
+> [!WARNING]  
+> There is a performance penalty with this approach.
+>
+> Use it only when necessary or if the performance tradeoff is acceptable for your site.
+
+## What is this?
+
+An exploration into dynamic dark mode. Some applications are large, old and burdened by a lack of standards which makes supporting a dark mode theme harder than it needs to be. This project attempts to remedy that by providing an automatic dark mode while the hard work of refactoring the codebase, so a more performant and standard approach to themeing can be developed, is underway.
---
+++ src/dynamic.ts
@@ -0,0 +1,391 @@
+import { colord, extend } from 'colord'
+import type { Plugin } from 'colord'
+import a11yPlugin from 'colord/plugins/a11y'
+
+type LightSwitchOptions = {
+  verbose?: boolean
+  /**
+   * The root element of the page where dark mode should be applied.
+   * It is recommended to use `document.body` for better performance.
+   * (although document.documentElement is also supported).
+   */
+  pageRoot?: HTMLElement
+  cssStyleSheet?: CSSStyleSheet
+  htmlFilterTags?: string[]
+  preserveHoverBorderColor?: boolean
+}
+class LightSwitch {
+  #isEnabled = false
+  #sheet = new CSSStyleSheet()
+  #elements = new Set<HTMLElement>()
+  #observers = new Map<Element, MutationObserver>()
+  #shadowRoots = new Set<ShadowRoot>()
+  #shadowSheets = new Set<CSSStyleSheet>()
+  #hoverColorRules = new Map<string, CSSRule>()
+  #htmlFilterSet: Set<string> = new Set([])
+  #pageRoot = document.body
+  #pageObserver = new MutationObserver(() => {
+    this.#update(this.#pageRoot)
+  })
+  #preserveHoverBorderColor = false
+  #backgroundColor = ''
+  #markStart = 'lightSwitch:start'
+  #markEnd = 'lightSwitch:end'
+  #verbose = false
+  #selector = ''
+  #cssRules = ''
+
+  static defaultCssRules = `
+    ::-webkit-scrollbar {
+      background-color: #333;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: #555;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: #333;
+    }
+    ::-webkit-scrollbar-corner {
+      background-color: #333;
+    }
+    ::-webkit-scrollbar-button {
+      background-color: #333;
+    }
+    ::-webkit-scrollbar-thumb:hover {
+      background-color: #777;
+    }
+    ::-webkit-scrollbar-thumb:active {
+      background-color: #999;
+    }
+    ::-webkit-scrollbar-track:hover {
+      background-color: #444;
+    }
+    ::-webkit-scrollbar-track:active {
+      background-color: #555;
+    }
+    ::-webkit-scrollbar-button:hover {
+      background-color: #444;
+    }
+    ::-webkit-scrollbar-button:active {
+      background-color: #555;
+    }
+    ::-webkit-scrollbar-corner:hover {
+      background-color: #444;
+    }
+    ::-webkit-scrollbar-corner:active {
+      background-color: #555;
+    }
+    * {
+      scrollbar-color: #333 #555 !important;
+    }
+    input[type="checkbox"], input[type="radio"] {
+      filter: invert(100%) hue-rotate(180deg) brightness(95%) contrast(20%);
+    }
+    input[type="checkbox"]:checked,
+    input[type="radio"]:checked {
+      filter: none !important;
+    }
+    img[src$=".png"], img[src$=".svg"] {
+      filter: invert(100%) hue-rotate(180deg) brightness(95%) contrast(70%);
+    }
+  `
+
+  constructor(options: LightSwitchOptions = {}) {
+    const {
+      cssStyleSheet,
+      htmlFilterTags,
+      verbose = false,
+      pageRoot = this.#pageRoot,
+      preserveHoverBorderColor = this.#preserveHoverBorderColor,
+    } = options
+
+    if (!window) {
+      throw new Error('LightSwitch requires a window to be initialized.')
+    }
+
+    this.#verbose = verbose
+    this.#pageRoot = pageRoot
+    this.#preserveHoverBorderColor = preserveHoverBorderColor
+    this.#htmlFilterSet = Array.isArray(htmlFilterTags)
+      ? new Set(htmlFilterTags.map(tag => tag.toLowerCase()))
+      : new Set([
+          'html',
+          'head',
+          'title',
+          'meta',
+          'script',
+          'style',
+          'link',
+          'iframe',
+          'svg',
+          'img',
+          'canvas',
+          'video',
+          'audio',
+        ])
+    this.#selector = Array.from(this.#htmlFilterSet)
+      .filter(tag => !tag.startsWith(':'))
+      .map(tag => `:not(${tag})`)
+      .join('')
+
+    if (cssStyleSheet instanceof CSSStyleSheet) {
+      this.#cssRules = Array.from(cssStyleSheet.cssRules)
+        .map(rule => rule.cssText)
+        .join('\n')
+    } else {
+      this.#cssRules = LightSwitch.defaultCssRules.replace('*', this.#selector)
+    }
+
+    extend([a11yPlugin as unknown as Plugin])
+  }
+
+  #log(root: Element) {
+    // eslint-disable-next-line no-console
+    console.log(
+      `${root.tagName} styles updated in `,
+      performance.measure('lightSwitch:update', this.#markStart, this.#markEnd).duration,
+      'ms',
+    )
+  }
+
+  set pageRoot(root: HTMLElement) {
+    if (!(root instanceof HTMLElement)) {
+      throw new Error('pageRoot must be an instance of HTMLElement')
+    }
+
+    this.#pageRoot = root
+    if (this.#isEnabled) {
+      this.#pageObserver.disconnect()
+      this.#pageObserver.observe(this.#pageRoot, {
+        childList: true,
+        subtree: true,
+      })
+    }
+  }
+
+  get backgroundColor() {
+    return this.#backgroundColor
+  }
+
+  #traverse(node: Node) {
+    if (node instanceof HTMLElement) {
+      this.#elements.add(node)
+
+      if (node.shadowRoot) {
+        if (!this.#observers.has(node.shadowRoot.host)) {
+          const observer = new MutationObserver(() => {
+            requestAnimationFrame(() => {
+              if (node.shadowRoot) {
+                this.#update(node.shadowRoot.host)
+              }
+            })
+          })
+          observer.observe(node.shadowRoot, {
+            childList: true,
+            subtree: true,
+          })
+          this.#observers.set(node.shadowRoot.host, observer)
+        }
+
+        this.#shadowRoots.add(node.shadowRoot)
+        ;[...node.shadowRoot.styleSheets]
+          .filter(sheet => {
+            return !sheet.href || sheet.href.startsWith(window.location.origin)
+          })
+          .forEach(sheet => {
+            this.#shadowSheets.add(sheet)
+          })
+        node.shadowRoot.querySelectorAll('*').forEach(el => {
+          this.#traverse(el)
+        })
+      }
+    }
+  }
+
+  #getHoverBorderColorRules() {
+    ;[...document.styleSheets, ...this.#shadowSheets]
+      .filter(sheet => {
+        return !sheet.href || sheet.href.startsWith(window.location.origin)
+      })
+      .flatMap(sheet => {
+        return [...sheet.cssRules]
+          .filter(rule => {
+            return rule instanceof CSSStyleRule || rule instanceof CSSMediaRule
+          })
+          .flatMap(rule => {
+            if (rule instanceof CSSStyleRule) {
+              return rule
+            }
+
+            return [...rule.cssRules].filter(r => r instanceof CSSStyleRule)
+          })
+          .filter(rule => {
+            return /:hover/.test(rule.selectorText) && /border/.test(rule.cssText)
+          })
+      })
+      .forEach(rule => {
+        this.#hoverColorRules.set(rule.selectorText, rule)
+      })
+  }
+
+  #walk(root: Element) {
+    if (root instanceof HTMLElement) {
+      this.#elements.add(root)
+    }
+
+    ;(root.shadowRoot || root).querySelectorAll('*').forEach(el => {
+      this.#traverse(el)
+    })
+
+    if (this.#preserveHoverBorderColor) {
+      this.#getHoverBorderColorRules()
+    }
+  }
+
+  #darken(el: HTMLElement) {
+    const computedStyle = getComputedStyle(el)
+    const bgColor = computedStyle.backgroundColor
+    const bgColord = colord(bgColor)
+    const tagName = el.tagName.toLowerCase()
+    let darkBgColor = bgColor
+
+    if (!Array.from(this.#htmlFilterSet).some(selector => el.matches(selector))) {
+      if (tagName === this.#pageRoot.tagName.toLowerCase()) {
+        darkBgColor = bgColord.darken(0.9).toHex()
+
+        if (!this.#backgroundColor) {
+          this.#backgroundColor = darkBgColor
+        }
+      } else if (!bgColord.isDark()) {
+        darkBgColor = bgColord.darken(0.9).toHex()
+      }
+
+      const borderColorAlpha = colord(computedStyle.borderColor).alpha()
+      let colored = colord(computedStyle.color)
+      let readable = colored.isReadable(darkBgColor)
+      let readableAttempts = 0
+
+      while (!readable && readableAttempts < 10) {
+        colored = colored.lighten(0.05)
+        readable = colored.isReadable(darkBgColor, { level: 'AAA' })
+        readableAttempts++
+      }
+
+      if (el.dataset.lightSwitch !== 'true') {
+        const originalColor = el.style.color
+        const originalBgColor = el.style.backgroundColor
+
+        if (originalColor) {
+          el.dataset.originalColor = originalColor
+        }
+
+        if (originalBgColor) {
+          el.dataset.originalBgColor = originalBgColor
+        }
+
+        if (borderColorAlpha > 0) {
+          const hoverRule = Array.from(this.#hoverColorRules.entries()).find(
+            ([selector]) => {
+              return el.matches(selector.replace(/:hover/g, ''))
+            },
+          )
+
+          if (!hoverRule) {
+            if (el.style.borderColor) {
+              el.dataset.originalBorderColor = el.style.borderColor
+            }
+            el.style.setProperty('border-color', colored.darken(0.45).toHex())
+          }
+        }
+
+        el.style.setProperty('color', colored.toHex())
+        el.style.setProperty('background-color', darkBgColor)
+        el.dataset.lightSwitch = 'true'
+      }
+    }
+  }
+
+  #undarken(el: HTMLElement) {
+    if (el.dataset.lightSwitch === 'true') {
+      delete el.dataset.lightSwitch
+      if (el.dataset.originalColor) {
+        el.style.setProperty('color', el.dataset.originalColor)
+        delete el.dataset.originalColor
+      } else {
+        el.style.removeProperty('color')
+      }
+      if (el.dataset.originalBgColor) {
+        el.style.setProperty('background-color', el.dataset.originalBgColor)
+        delete el.dataset.originalBgColor
+      } else {
+        el.style.removeProperty('background-color')
+      }
+      if (el.dataset.originalBorderColor) {
+        el.style.setProperty('border-color', el.dataset.originalBorderColor)
+        delete el.dataset.originalBorderColor
+      } else {
+        el.style.removeProperty('border-color')
+      }
+    }
+  }
+
+  /**
+   * Prevent memory leaks by clearing all elements, shadow roots, and observers.
+   * This is called when the dark mode is reset or when the page is updated.
+   * It also disconnects any observers that are no longer connected to the DOM.
+   */
+  #clear() {
+    this.#elements.clear()
+    this.#shadowRoots.clear()
+    this.#shadowSheets.clear()
+    this.#hoverColorRules.clear()
+    this.#observers.forEach((observer, element) => {
+      if (!element.isConnected) {
+        observer.disconnect()
+        this.#observers.delete(element)
+      }
+    })
+  }
+
+  #update(root: Element) {
+    performance.mark(this.#markStart)
+    this.#clear()
+    this.#walk(root)
+    this.#elements.forEach(this.#darken.bind(this))
+    this.#shadowRoots.forEach(shadowRoot => {
+      shadowRoot.adoptedStyleSheets = [...shadowRoot.adoptedStyleSheets, this.#sheet]
+    })
+    performance.mark(this.#markEnd)
+
+    if (this.#verbose) {
+      this.#log(root)
+    }
+  }
+
+  off() {
+    this.#isEnabled = true
+    this.#pageObserver.observe(this.#pageRoot, {
+      childList: true,
+      subtree: true,
+    })
+    this.#sheet.replaceSync(this.#cssRules)
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, this.#sheet]
+    this.#update(this.#pageRoot)
+  }
+
+  on() {
+    this.#isEnabled = false
+    this.#walk(this.#pageRoot)
+    this.#sheet.replaceSync('')
+    this.#backgroundColor = ''
+    this.#elements.forEach(this.#undarken.bind(this))
+    this.#pageObserver.disconnect()
+    this.#observers.forEach((observer, element) => {
+      observer.disconnect()
+      this.#observers.delete(element)
+    })
+    this.#clear()
+  }
+}
+
+export { LightSwitch }
